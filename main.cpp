0ab4fcd29c0fe1ad8524a126333586e73c05f61b
harunmohamed <harunmohamed901@gmail.com>
2022-08-31 22:01:18 +0300

    script to run c++ files

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int num;
+    cout << "enter your favorite number: ";
+    cin >> num;
+    cout << "hey there, " << num << " is also my favorite number!";
+
+    return 0;
+}
---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+## shell script to compile and run a C++ program using g++ compiler
+
+g++ -Wall -std=c++17 main.cpp -o main
+./main
